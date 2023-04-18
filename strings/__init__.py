883230214958c015511ab8d3feabc63a7bdb54fb
vickyvasnth13 <67225663+vickyvasnth13@users.noreply.github.com>
2023-04-18 21:45:39 +0530

    Add files via upload

--- strings/__init__.py
+++ strings/__init__.py
@@ -37,24 +37,24 @@ for filename in os.listdir(r"./strings"):
 
 
 for filename in os.listdir(r"./strings/langs/"):
-    if "vicky" not in languages:
-        languages["vicky"] = yaml.safe_load(
-            open(r"./strings/langs/vicky.yml", encoding="utf8")
+    if "en" not in languages:
+        languages["en"] = yaml.safe_load(
+            open(r"./strings/langs/en.yml", encoding="utf8")
         )
-        languages_present["vicky"] = languages["vicky"]["name"]
+        languages_present["en"] = languages["en"]["name"]
     if filename.endswith(".yml"):
         language_name = filename[:-4]
-        if language_name == "vicky":
+        if language_name == "en":
             continue
         languages[language_name] = yaml.safe_load(
             open(r"./strings/langs/" + filename, encoding="utf8")
         )
-        for item in languages["vicky"]:
+        for item in languages["en"]:
             if item not in languages[language_name]:
-                languages[language_name][item] = languages["vicky"][item]
+                languages[language_name][item] = languages["en"][item]
     try:
         languages_present[language_name] = languages[language_name][
-            "vicky"
+            "name"
         ]
     except:
         print(
