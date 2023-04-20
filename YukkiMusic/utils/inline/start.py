11f2ff06aa40b969cb917dd71d2ca7977685d881
vickyvasnth13 <67225663+vickyvasnth13@users.noreply.github.com>
2023-04-20 15:39:02 +0530

    Update start.py

--- YukkiMusic/utils/inline/start.py
+++ YukkiMusic/utils/inline/start.py
@@ -94,14 +94,6 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
                     )
                 ]
             )
-    buttons.append(
-        [
-            InlineKeyboardButton(
-                text=_["S_B_5"],
-                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
-            )
-        ]
-    )
     if GITHUB_REPO and OWNER:
         buttons.append(
             [
