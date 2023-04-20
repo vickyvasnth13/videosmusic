6debac0fe1698007b80207c9ad3331f9f26f30d8
Buttu <129733301+iButtu@users.noreply.github.com>
2023-04-20 16:52:46 +0530

    Update start.py

--- YukkiMusic/utils/inline/start.py
+++ YukkiMusic/utils/inline/start.py
@@ -25,10 +25,6 @@ def start_pannel(_):
             InlineKeyboardButton(
                 text=_["S_B_2"], callback_data="settings_helper"
             ),
-             InlineKeyboardButton(
-                text=_["💥 ʜᴇʟᴘ 🕵️"],
-                url=f"https://t.me/Music_vibing_bot?start=help",
-            ),
         ],
     ]
     if SUPPORT_CHANNEL and SUPPORT_GROUP:
