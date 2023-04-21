87231ff33087f01bff67b8d09d7e05bcaf386868
vickyvasnth13 <67225663+vickyvasnth13@users.noreply.github.com>
2023-04-21 19:37:40 +0530

    Update help.py

--- YukkiMusic/plugins/bot/help.py
+++ YukkiMusic/plugins/bot/help.py
@@ -122,63 +122,63 @@ async def helper_cb(client, CallbackQuery, _):
         await CallbackQuery.edit_message_text(
             helpers.HELP_4, reply_markup=keyboard
         )
-         elif cb == "hb5":
+    elif cb == "hb5":
         await CallbackQuery.edit_message_text(
             helpers.HELP_5, reply_markup=keyboard
         )
-         elif cb == "hb6":
+    elif cb == "hb6":
         await CallbackQuery.edit_message_text(
             helpers.HELP_6, reply_markup=keyboard
         )
-         elif cb == "hb7":
+    elif cb == "hb7":
         await CallbackQuery.edit_message_text(
             helpers.HELP_7, reply_markup=keyboard
         )
-         elif cb == "hb8":
+    elif cb == "hb8":
         await CallbackQuery.edit_message_text(
             helpers.HELP_8, reply_markup=keyboard
         )
-         elif cb == "hb9":
+    elif cb == "hb9":
         await CallbackQuery.edit_message_text(
             helpers.HELP_9, reply_markup=keyboard
         )
-         elif cb == "hb10":
+    elif cb == "hb10":
         await CallbackQuery.edit_message_text(
             helpers.HELP_10, reply_markup=keyboard
         )
-         elif cb == "hb11":
+    elif cb == "hb11":
         await CallbackQuery.edit_message_text(
             helpers.HELP_11, reply_markup=keyboard
         )
-         elif cb == "hb12":
+    elif cb == "hb12":
         await CallbackQuery.edit_message_text(
             helpers.HELP_12, reply_markup=keyboard
         )
-         elif cb == "hb13":
+    elif cb == "hb13":
         await CallbackQuery.edit_message_text(
             helpers.HELP_13, reply_markup=keyboard
         )
-         elif cb == "hb14":
+    elif cb == "hb14":
         await CallbackQuery.edit_message_text(
             helpers.HELP_14, reply_markup=keyboard
         )
-         elif cb == "hb15":
+    elif cb == "hb15":
         await CallbackQuery.edit_message_text(
             helpers.HELP_15, reply_markup=keyboard
         )
-         elif cb == "hb16":
+    elif cb == "hb16":
         await CallbackQuery.edit_message_text(
             helpers.HELP_16, reply_markup=keyboard
         )
-         elif cb == "hb17":
+    elif cb == "hb17":
         await CallbackQuery.edit_message_text(
             helpers.HELP_17, reply_markup=keyboard
         )
-         elif cb == "hb18":
+    elif cb == "hb18":
         await CallbackQuery.edit_message_text(
             helpers.HELP_18, reply_markup=keyboard
         )
-         elif cb == "hb19":
+    elif cb == "hb19":
         await CallbackQuery.edit_message_text(
             helpers.HELP_19, reply_markup=keyboard
         )
