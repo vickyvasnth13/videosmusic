53c408121e6a64dfa33bfc5d1e1475b16f3a91cd
˹ᴧɴσɴʏᴍσᴜs ꭙ˼ <TheAnonymousX1025@gmail.com>
2023-04-23 12:43:09 +0530

    Update help.py

--- YukkiMusic/plugins/bot/help.py
+++ YukkiMusic/plugins/bot/help.py
@@ -50,15 +50,9 @@ async def helper_private(
         language = await get_lang(chat_id)
         _ = get_string(language)
         keyboard = help_pannel(_, True)
-        if update.message.photo:
-            await update.message.delete()
-            await update.message.reply_text(
-                _["help_1"], reply_markup=keyboard
-            )
-        else:
-            await update.edit_message_text(
-                _["help_1"], reply_markup=keyboard
-            )
+        await update.edit_message_text(
+            _["help_1"], reply_markup=keyboard
+        )
     else:
         chat_id = update.chat.id
         if await is_commanddelete_on(update.chat.id):
