60e7ab000bdbf492e5f32dce615f3b5661f4abf7
vickyvasnth13 <67225663+vickyvasnth13@users.noreply.github.com>
2023-05-01 16:54:22 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -76,7 +76,7 @@ AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", None)
 
 # Time after which you're assistant account will leave chats automatically.
 AUTO_LEAVE_ASSISTANT_TIME = int(
-    getenv("ASSISTANT_LEAVE_TIME", "5400")
+    getenv("ASSISTANT_LEAVE_TIME", "300")
 )  # Remember to give value in Seconds
 
 # Time after which bot will suggest random chats about bot commands.
